e26bdab2b808aba2d3ecc7d812d71efeb0619547
Lewis <dsapione@gmail.com>
2022-09-21 23:42:42 -0400

    font awesome added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
-      </p>
-      <p className="text-gray-500 text-lg">
-        React and Tailwind CSS in action
-      </p>
-    </div>
+		// <!-- header containg name and icons -->
+		<header class="flex flex-col justify-center items-center h-screen">
+			<h1 class="title text-8xl font-bold">Daniel Sapione</h1>
+			<p class="text-xl -translate-y-2" >FRONT END DEVELOPER | PORTFOLIO</p>
+			{/* <!-- line div between name and icons --> */}
+			<div class="border-b-[6px] border-solid border-slate-500 rounded-[3px] w-[26em]"></div>
+			{/* <!-- icons --> */}
+			<section class="flex justify-evenly w-[10em] mt-[12.5px] text-5xl">
+			<a href="https://github.com/dsapione" target="blank">
+				<i class="fa-brands fa-square-github transition duration-500 hover:text-slate-500 hover:ease-in-out"></i>
+			</a>
+			<a href="mailto:dsapion@gmail.com">
+				<i class="fa-solid fa-envelope transition duration-500 hover:text-slate-500 hover:ease-in-out"></i>
+			</a>
+			<a href="https://www.linkedin.com/in/daniel-sapione/" target="_blank" rel="noreferrer">
+				<i class="fa-brands fa-linkedin transition duration-500 hover:text-slate-500 hover:ease-in-out"></i>
+			</a>
+			</section>
+		</header>
   );
 }
 
