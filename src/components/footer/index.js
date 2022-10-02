c3b5a20180c8f01cfcb66a165181aceaf6ce7beb
dsapione <dsapione@gmail.com>
2022-10-02 17:22:21 -0400

    refactored, removed garbage flowbite

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,11 +4,11 @@ import React from 'react';
 function Footer() {
 	return (
 		// brings you to top of page
-		<footer className='flex justify-center clear-both'>
+		<footer className="flex justify-center clear-both mt-3">
 			<a className="duration-500 opacity-40 hover:opacity-80 hover:ease-in-out" href="#">
-			<i className="fa-solid fa-chevron-up text-3xl"></i>
-			<p className="flex justify-center text-sm -translate-y-2">Top</p>
-		</a>
+				<i className="fa-solid fa-chevron-up text-3xl"></i>
+				<p className="flex justify-center text-sm -translate-y-2">Top</p>
+			</a>
 		</footer>
 	);
 };
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 function Header () {
 	return (
-				// header containg name and icons
+			// header containg name and icons
 			<header className="flex flex-col justify-center items-center h-screen">
-				<h1 className="title text-8xl font-bold">Daniel Sapione</h1>
+				<h1 className="title text-7xl md:text-8xl font-bold">Daniel Sapione</h1>
 				<p className="text-xl -translate-y-2" >FRONT END DEVELOPER | PORTFOLIO</p>
 				{/* line span between name and icons */}
-				<span className="border-b-[6px] border-solid border-sky-500 rounded-[3px] w-[26em]"></span>
+				<span className="border-b-[6px] border-solid border-sky-500 rounded-[3px] w-[20em] md:w-[26em]"></span>
 				{/* icons */}
-				<section className="flex justify-evenly w-[10em] mt-[12.5px] text-5xl">
-					<a href="https://github.com/dsapione" target="blank">
+				<section className="flex justify-evenly w-[9em] md:w-[10em] mt-[12.5px] text-5xl">
+					<a href="https://github.com/dsapione" target="_blank" rel="noreferrer">
 						<i className="fa-brands fa-square-github duration-500 hover:text-sky-500 hover:ease-in-out"></i>
 					</a>
 					<a href="mailto:dsapion@gmail.com">
